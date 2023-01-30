7ef848a36c8191bccd6d9be9792ef945ff9d1240
shaikat17 <shaikatpal56@gmail.com>
2023-01-30 12:06:54 +0600

    working on pagination project using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// Imported Files
+
 
 function App() {
+  const { loading, data } = useFetch()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Pagination Project With React</h1>
+    </>
   );
 }
 
