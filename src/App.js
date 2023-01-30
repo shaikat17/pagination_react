ff977cd5a89f74a3e57328891ad199681c0867cd
shaikat17 <shaikatpal56@gmail.com>
2023-01-30 23:06:32 +0600

    data slice for page numbers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 // Imported Files
-
+import { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 
 function App() {
-  const { loading, data } = useFetch()
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading]);
+
   return (
-    <>
-    <h1>Pagination Project With React</h1>
-    </>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "Pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower}></Follower>;
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
